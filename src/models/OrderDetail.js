fee2f5faa04991adbdc9d6add3d09d23a805d268
Juanitho007 <juan041008dyb@gmail.com>
2024-09-19 17:04:32 -0600

    create model roles

---
+++ src/models/OrderDetail.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+
+const OrderDetail = sequelize.define(
+  "orderDetail",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    product: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    value: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    unitOfMeasurement: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    currencyCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "orderDetail",
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: false,
+  }
+);
+module.exports = OrderDetail;
---
+++ src/models/Stock.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+
+const Stock = sequelize.define(
+  "stock",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    companyId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sku: {
+      type: DataTypes.STRING,
+      comment: "16 caracteres",
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+      comment: "Disponibilidad",
+    },
+  },
+  {
+    tableName: "stock",
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: false,
+  }
+);
+module.exports = Stock;
