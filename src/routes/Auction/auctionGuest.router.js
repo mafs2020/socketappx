2d83a284eff629c92d012c0586584a73ad5724de
martin <martin117h@gmail.com>
2024-10-30 17:26:51 -0600

    se modifica el nombre de la ruta porque se repetia

--- src/routes/Auction/auctionGuest.router.js
+++ src/routes/Auction/auctionGuest.router.js
@@ -1,16 +1,20 @@
-const { getAll, create, getOne, remove, update 
-} = require('../../controllers/Auction/auctionGuest.controller');
-const express = require('express');
+const {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+} = require("../../controllers/Auction/auctionGuest.controller");
+const express = require("express");
 
 const auctionGuestRouter = express.Router();
 
-auctionGuestRouter.route('/auction')
-    .get(getAll)
-    .post(create);
+auctionGuestRouter.route("/auctionguest").get(getAll).post(create);
 
-auctionGuestRouter.route('/auction/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+auctionGuestRouter
+  .route("/auctionguest/:id")
+  .get(getOne)
+  .delete(remove)
+  .put(update);
 
 module.exports = auctionGuestRouter;
