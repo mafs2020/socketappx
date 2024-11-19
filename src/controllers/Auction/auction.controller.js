fe59d425118cd99ac96d7add5fced860b6f5b533
martin <martin117h@gmail.com>
2024-11-19 13:52:11 -0600

    se agrega cambios para la creacion de la subasta(el producto esta hardcodeado)

--- src/controllers/Auction/auction.controller.js
+++ src/controllers/Auction/auction.controller.js
@@ -1,89 +1,87 @@
-const catchError = require('../../utils/catchError');
-const Auction = require('../../models/Auction/Auction');
-const AuctionGuest = require('../../models/Auction/AuctionGuest');
-const GuestBid = require('../../models/Auction/GuestBid');
+const catchError = require("../../utils/catchError");
+const Auction = require("../../models/Auction/Auction");
+const AuctionGuest = require("../../models/Auction/AuctionGuest");
+const GuestBid = require("../../models/Auction/GuestBid");
 
-const getAll = catchError(async(req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 20;
-    const offset = (page - 1) * pageSize;
-    const results = await Auction.findAndCountAll({
-        limit: pageSize,
-        offset: offset,
-        include: [
-            { model: AuctionGuest },
-        ],
-    });
-    const response = {
+const getAll = catchError(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 20;
+  const offset = (page - 1) * pageSize;
+  const results = await Auction.findAndCountAll({
+    limit: pageSize,
+    offset: offset,
+    include: [{ model: AuctionGuest }],
+  });
+  const response = {
     totalRecords: results.count,
     totalPages: Math.ceil(results.count / pageSize),
     currentPage: page,
     pageSize: pageSize,
     data: results.rows,
-    };
-    return res.json(response)
+  };
+  return res.json(response);
 });
 
-
-const getAllWinner = catchError(async(req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 20;
-    const offset = (page - 1) * pageSize;
-    const results = await Auction.findAndCountAll({
-        limit: pageSize,
-        offset: offset,
-        include: [
-            { model: AuctionGuest },
-            { model: GuestBid, where: {winnerBid: true} },
-        ],
-    });
-    const response = {
+const getAllWinner = catchError(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 20;
+  const offset = (page - 1) * pageSize;
+  const results = await Auction.findAndCountAll({
+    limit: pageSize,
+    offset: offset,
+    include: [
+      { model: AuctionGuest },
+      { model: GuestBid, where: { winnerBid: true } },
+    ],
+  });
+  const response = {
     totalRecords: results.count,
     totalPages: Math.ceil(results.count / pageSize),
     currentPage: page,
     pageSize: pageSize,
     data: results.rows,
-    };
-    return res.json(response)
+  };
+  return res.json(response);
 });
 
-const create = catchError(async(req, res) => {
-    let body = req.body;
-    const result = await Auction.create({...body});
-    return res.status(201).json(result);
+const create = catchError(async (req, res) => {
+  let body = req.body;
+  //   return res.status(404).json({ error: "este es un error desde el backend" });
+  return res.status(201).json({ ok: "auction Controller" });
+  const result = await Auction.create({ ...body });
 });
 
-const getOne = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await Auction.findByPk({id, 
-        include: [
-        { model: AuctionGuest },
-    ]});
-    if(!result) return res.sendStatus(404);
-    return res.json(result);
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await Auction.findByPk({
+    id,
+    include: [{ model: AuctionGuest }],
+  });
+  if (!result) return res.sendStatus(404);
+  return res.json(result);
 });
 
-const remove = catchError(async(req, res) => {
-    const { id } = req.params;
-    await Auction.destroy({ where: {id} });
-    return res.sendStatus(204);
+const remove = catchError(async (req, res) => {
+  const { id } = req.params;
+  await Auction.destroy({ where: { id } });
+  return res.sendStatus(204);
 });
 
-const update = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await Auction.update(
-        req.body,
-        { where: {id}, returning: true }
-    );
-    if(result[0] === 0) return res.sendStatus(404);
-    return res.json(result[1][0]);
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await Auction.update(req.body, {
+    where: { id },
+    returning: true,
+  });
+  if (result[0] === 0) return res.sendStatus(404);
+  return res.json(result[1][0]);
 });
 
 module.exports = {
-    getAll,
-    create,
-    getOne,
-    remove,
-    update,
-    getAllWinner,
-}
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+  getAllWinner,
+};
--- src/routes/Address/address.router.js
+++ src/routes/Address/address.router.js
@@ -1,24 +1,34 @@
-const { getAll, create, getOne, remove, update, getAllByUserID, createUserId, updateMainAddressCompany, updateMainAddressUser,
-    getAllByCompanyID, createCompanyId
-} = require('../../controllers/Address/address.controller');
-const express = require('express');
+const {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+  getAllByUserID,
+  createUserId,
+  updateMainAddressCompany,
+  updateMainAddressUser,
+  getAllByCompanyID,
+  createCompanyId,
+  createAddressAuction,
+} = require("../../controllers/Address/address.controller");
+const express = require("express");
 
 const addressRouter = express.Router();
 
-addressRouter.route('/address')
-    .get(getAll)
-    .post(create);
+addressRouter.route("/address").get(getAll).post(create);
 
-addressRouter.route('/address/:id')
-    .get(getOne)
-    .delete(remove)
-    .put(update);
+addressRouter.route("/addressauction").post(createAddressAuction);
 
-addressRouter.route('/address/userid').post(getAllByUserID)
-addressRouter.route('/address/create/userid').post(createUserId)
-addressRouter.route('/address/update/userid/:id').put(updateMainAddressUser)
-addressRouter.route('/address/update/companyId/:id').put(updateMainAddressCompany)
-addressRouter.route('/address/companyId').post(getAllByCompanyID)
-addressRouter.route('/address/create/companyid').post(createCompanyId)
+addressRouter.route("/address/:id").get(getOne).delete(remove).put(update);
+
+addressRouter.route("/address/userid").post(getAllByUserID);
+addressRouter.route("/address/create/userid").post(createUserId);
+addressRouter.route("/address/update/userid/:id").put(updateMainAddressUser);
+addressRouter
+  .route("/address/update/companyId/:id")
+  .put(updateMainAddressCompany);
+addressRouter.route("/address/companyId").post(getAllByCompanyID);
+addressRouter.route("/address/create/companyid").post(createCompanyId);
 
 module.exports = addressRouter;
