db5f1ecc353797e21489e45162fa2b26c984ae6d
martin <martin117h@gmail.com>
2024-11-26 16:34:14 -0600

    se agrega en el controller la funcion addGuest

--- src/controllers/Auction/auctionGuest.controller.js
+++ src/controllers/Auction/auctionGuest.controller.js
@@ -1,48 +1,58 @@
-const catchError = require('../../utils/catchError');
-const AuctionGuest = require('../../models/Auction/AuctionGuest')
+const catchError = require("../../utils/catchError");
+const AuctionGuest = require("../../models/Auction/AuctionGuest");
 
-const getAll = catchError(async(req, res) => {
-    //Recibe el id de la subasta
-    let auctionId = req.body.auctionId;
-    const results = await AuctionGuest.findAll({where: auctionId});
-    return res.json(results);
+const getAll = catchError(async (req, res) => {
+  //Recibe el id de la subasta
+  let auctionId = req.body.auctionId;
+  const results = await AuctionGuest.findAll({ where: auctionId });
+  return res.json(results);
 });
 
-const create = catchError(async(req, res) => {
-    //En el body debe de llevar la id de la subasta
-    const result = await AuctionGuest.create(req.body);
-    return res.status(201).json(result);
+const create = catchError(async (req, res) => {
+  //En el body debe de llevar la id de la subasta
+  const result = await AuctionGuest.create(req.body);
+  return res.status(201).json(result);
 });
 
-const getOne = catchError(async(req, res) => {
-    //Recibe el id de la subasta
-    let auctionId = req.body.auctionId;
-    const { id } = req.params;
-    const result = await AuctionGuest.findOne( {where: id, auctionId});
-    if(!result) return res.sendStatus(404);
-    return res.json(result);
+const getOne = catchError(async (req, res) => {
+  //Recibe el id de la subasta
+  let auctionId = req.body.auctionId;
+  const { id } = req.params;
+  const result = await AuctionGuest.findOne({ where: id, auctionId });
+  if (!result) return res.sendStatus(404);
+  return res.json(result);
 });
 
-const remove = catchError(async(req, res) => {
-    const { id } = req.params;
-    await AuctionGuest.destroy({ where: {id} });
-    return res.sendStatus(204);
+const remove = catchError(async (req, res) => {
+  const { id } = req.params;
+  await AuctionGuest.destroy({ where: { id } });
+  return res.sendStatus(204);
 });
 
-const update = catchError(async(req, res) => {
-    const { id } = req.params;
-    const result = await AuctionGuest.update(
-        req.body,
-        { where: {id}, returning: true }
-    );
-    if(result[0] === 0) return res.sendStatus(404);
-    return res.json(result[1][0]);
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await AuctionGuest.update(req.body, {
+    where: { id },
+    returning: true,
+  });
+  if (result[0] === 0) return res.sendStatus(404);
+  return res.json(result[1][0]);
+});
+const addGuest = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await AuctionGuest.update(req.body, {
+    where: { id },
+    returning: true,
+  });
+  if (result[0] === 0) return res.sendStatus(404);
+  return res.json(result[1][0]);
 });
 
 module.exports = {
-    getAll,
-    create,
-    getOne,
-    remove,
-    update
-}
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+  addGuest,
+};
