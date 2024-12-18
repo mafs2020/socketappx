dd925a5a755b4c19f2a2f9e8c40a6be86c9965a4
martin117h <martin117h@gmail.com>
2024-12-18 17:36:34 -0600

    se agrega el dialecto a la base de datos

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  host: "postgresql://postgres:KveNhoZnKjYlrKNRmdLPxVRzfqKlyHCw@autorack.proxy.rlwy.net:59373/railway",
+  database: "railway",
+  username: "postgres",
+  password: "KveNhoZnKjYlrKNRmdLPxVRzfqKlyHCw",
 });
 
 module.exports = sequelize;
